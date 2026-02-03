8639d8d21b2740e217b4bb36737577c107a9961c
edimarivas-prog <edimarivas@gmail.com>
2026-02-03 08:52:13 -0500

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,6 @@
 // js/supabase-config.js
 
-const SUPABASE_URL = 'https://wulnsvyyrfsaiartzypn.supabase.co'  // REEMPLAZA CON TU  URL
+const SUPABASE_URL = 'https://wulnsvyyrfsaiartzypn.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1bG5zdnl5cmZzYWlhcnR6eXBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNTQwNjgsImV4cCI6MjA4NTYzMDA2OH0.sa0JNGf3haLPz5poZBSMML6ydq3EJ1P84g0jpZf7Nv8'  // REEMPLAZA CON TU ANON KEY
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
