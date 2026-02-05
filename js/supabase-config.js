a2f10870cdc24f2e09fc4d648b09684629f6c30e
edimarivas-prog <edimarivas@gmail.com>
2026-02-05 11:52:27 -0500

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,16 +1,12 @@
-// js/supabase-config.js
+var _SB_URL = 'https://wulnsvyyrfsaiartzypn.supabase.co'; 
+var _SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1bG5zdnl5cmZzYWlhcnR6eXBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNTQwNjgsImV4cCI6MjA4NTYzMDA2OH0.sa0JNGf3haLPz5poZBSMML6ydq3EJ1P84g0jpZf7Nv8';
 
-// Usamos 'var' para que no explote si el script se carga dos veces
-var _SB_URL = 'https://wulnsvyyrfsaiartzypn.supabase.co'; // 🔴 PEGA TU URL DE SUPABASE
-var _SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1bG5zdnl5cmZzYWlhcnR6eXBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNTQwNjgsImV4cCI6MjA4NTYzMDA2OH0.sa0JNGf3haLPz5poZBSMML6ydq3EJ1P84g0jpZf7Nv8'; // 🔴 PEGA TU ANON KEY DE SUPABASE
-
-// Solo iniciamos si no existe ya 'window.sb'
+// Iniciamos solo si no existe ya
 if (!window.sb) {
     if (typeof supabase !== 'undefined') {
-        // Creamos el cliente y lo guardamos en 'sb'
         window.sb = supabase.createClient(_SB_URL, _SB_KEY);
-        console.log("✅ Supabase (window.sb) listo.");
+        console.log("✅ Conexión establecida: Bitácora Mental (PE)");
     } else {
-        console.error("❌ Error: La librería de Supabase no cargó.");
+        console.error("❌ Error crítico: Librería Supabase no encontrada.");
     }
 }
