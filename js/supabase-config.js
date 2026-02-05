9d1b6a1e98632b12352483d787e8405d4855d419
edimarivas-prog <edimarivas@gmail.com>
2026-02-05 11:19:17 -0500

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -5,8 +5,8 @@ if (!window.supabaseClient) {
     
     // USAMOS 'var' O DIRECTAMENTE LAS CADENAS PARA EVITAR EL ERROR DE "YA DECLARADO"
     // Reemplaza estos valores con tus credenciales reales de Supabase
-    var SUPABASE_URL = 'AQUI_TU_URL_DE_SUPABASE'; 
-    var SUPABASE_KEY = 'AQUI_TU_ANON_KEY_DE_SUPABASE';
+    var SUPABASE_URL = 'https://wulnsvyyrfsaiartzypn.supabase.co'; 
+    var SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1bG5zdnl5cmZzYWlhcnR6eXBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNTQwNjgsImV4cCI6MjA4NTYzMDA2OH0.sa0JNGf3haLPz5poZBSMML6ydq3EJ1P84g0jpZf7Nv8';
 
     // 2. Inicializamos el cliente
     // Asegúrate de que la librería de supabase-js se haya cargado antes que este script
